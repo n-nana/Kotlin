48cf31d397c1d05ca93ef33eb133b1be2ddc03cf
n_na <91656188+n-nana@users.noreply.github.com>
2023-06-16 09:25:49 +0900

    Update ABC296A.kt

--- ABC296A.kt
+++ ABC296A.kt
@@ -1,6 +1,16 @@
-//ABC296A - Alternately
-
 fun main() {
+//標準入力
+//    val N = readLine()!!.toInt()
+//    val S = readLine()!!
+//    val (N,M) = readLine()!!.split(" ").map{ it.toInt() }
+//    val A = (0 until M).map{readLine()!!.toInt()}
+//    val R:  List<Int> = A.reversed(); //reverse
+
+//配列初期化
+//    val res = BooleanArray(mx) {false}
+//    val cell = Array(T+3) { IntArray(3) {0}}
+//    val dp = Array(N+3) { BooleanArray(mx+3) {false}}
+
     val N = readLine()!!.toInt()
     val S = readLine()!!
     var ans = "Yes"
